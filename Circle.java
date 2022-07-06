af15cb68087229a5ebf9b88e819b3d563eb3d284
Rahul-2911 <107523775+Rahul-2911@users.noreply.github.com>
2022-07-06 14:16:42 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,16 @@
+
+class Circle
+{
+	float radius;
+
+	Circle() 
+	{
+		radius=1.5f;
+	}
+
+	public static void main(String[] args) {
+		Circle c = new Circle();
+     System.out.println("the radius of the circle is :"+c.radius);
+	}
+
+}
